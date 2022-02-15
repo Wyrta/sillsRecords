feddc5143cf8501af1ecdf2360c8ee0ed48d4610
Wyrta <81468617+Wyrta@users.noreply.github.com>
2022-02-15 11:56:55 +0100

    Add files via upload

---
+++ fr/index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="../global_style.css"/>
+        <link rel="stylesheet" type="text/css" href="./style.css"/>
+        <title>Sills Records</title>
+        <meta name="OUI" content="Site de Sills Records" />
+    </head>
+    <body class=bodyAcceuil>
+
+        <div class="contact"><a href="./contact-us/">Nous contacter</a></div>
+        <div class="language">
+            <a href="../fr/"> <button class="bp-fr-active">Fr</button>  </a>
+            <a style="font-size: 150%; letter-spacing: 0.03em; color: lightgray; font-weight: bold;">|</a>
+            <a href="../en/"> <button class="bp-en">En</button> </a>
+        </div>
+        
+        <img class="sillsLogo" src="../ressources/Sills_Records.jpg" alt="rip">
+        
+        <table class="choice" cellspacing="0" cellpadding="0">
+            <tr>
+                <td class="col-1" > <a href="./Stuff n' all/">Trucs et tout</a>          </td>
+                <td class="col-2" > <a href="./artist/">Artistes de la maison</a>   </td>
+                <td class="col-3" > <a href="./about-us/">A propos de nous</a>      </td>
+            </tr>
+        </table>
+
+    </body>
+
+</html>
